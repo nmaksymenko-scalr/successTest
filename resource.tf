cf4cf1f49fd66d5915eb6234917c64b7dcfbf511
Nikolay Maksymenko <89513940+nmaksymenko-scalr@users.noreply.github.com>
2021-12-30 12:47:54 +0200

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -4,10 +4,31 @@ provider "null" {
 resource "null_resource" "resource_name" {
 }
 
+resource "null_resource" "resource_name1" {
+}
+
+resource "null_resource" "resource_name2" {
+}
+
+resource "null_resource" "resource_name3" {
+}
+
 variable "test_input" {
   default="defaut_value"
 }
 
 output "test_output" {
   value="ouput_value"
+}
+
+output "test_output1" {
+  value="ouput_value1"
+}
+
+output "test_output2" {
+  value="ouput_value2"
+}
+
+output "test_output3" {
+  value="ouput_value3"
 }
